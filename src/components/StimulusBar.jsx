0c2344f93e6836f4e9041350d7becc8671b3f4aa
TimeGlitch <frankj.zhang@gmail.com>
2025-09-19 15:32:03 -0700

    tweakin

--- src/components/StimulusBar.jsx
+++ src/components/StimulusBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import styles from '../styles'
 
-const StimulusBar = ({ src, audioElementRef }) => {
+const StimulusBar = ({ src, audioElementRef, message }) => {
   return (
     <>
+    {message || "Click play to hear the stimulus."}
     <audio controls ref={audioElementRef} style={styles.audioPlayer}>
         <source src={src} type="audio/mpeg" style={{ width: '100%' }} />
         Your browser does not support the audio element.
